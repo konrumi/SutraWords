88dc93080e39c5a3c9319870b411311385129bb9
Konrumi <konrumi@gmail.com>
2021-01-13 00:18:52 +0800

    :sparkles: add styles

--- public/static/index.js
+++ public/static/index.js
@@ -7,22 +7,38 @@
   const eleFileInput = document.getElementById('fileInput')
   const eleDividerSelect = document.getElementById('dividerSelect')
   const eleSubmitButton = document.getElementById('submitButton')
+  const eleDownloadButton = document.getElementById('downloadButton')
   const eleResultTable = document.getElementById('resultTable')
   const eleResultContainer = eleResultTable.getElementsByTagName('tbody')[0]
+  const eleFox = document.getElementById('fox')
+  const eleFoxImgs = eleFox.getElementsByTagName('img')
 
   // File reader
   eleFileInput.addEventListener('change', function (event) {
+    setFox(1)
     file = event.target.files[0]
 
     // Check Files
     if (!file) {
-      alert('请上传单个文本文件。')
+      alert('文件不符合规范，请上传单个文本文件。')
+      file = null
+      text = ''
+      eleResultContainer.innerHTML = ''
+      eleDownloadButton.disabled = true
+      eleDownloadButton.onclick = null
+      setFox(0)
       return
     }
 
     // Check FileReader
     if (!FileReader) {
-      alert('环境不支持读取文件，请使用 Chrome 7+ / Edge 12+ / Firefox 4+ 等浏览器。')
+      alert('当前环境不支持读取文件，请使用 Chrome 7+ / Edge 12+ / Firefox 4+ 等浏览器。')
+      file = null
+      text = ''
+      eleResultContainer.innerHTML = ''
+      eleDownloadButton.disabled = true
+      eleDownloadButton.onclick = null
+      setFox(0)
       return
     }
 
@@ -40,6 +56,18 @@
 
   // Submit process
   eleSubmitButton.addEventListener('click', function () {
+    setFox(2)
+    eleResultContainer.innerHTML = ''
+    eleDownloadButton.disabled = true
+    eleDownloadButton.onclick = null
+
+    // Check text
+    if (text.length === 0) {
+      alert('解析文本为空。')
+      setFox(0)
+      return
+    }
+
     let divider = eleDividerSelect.value
     let textArray = text.split(divider)
 
@@ -61,7 +89,7 @@
       const worker = new Worker('./static/worker.js')
 
       worker.onmessage = function (e) {
-        renderResult(e.data)
+        generateResult(e.data)
       }
 
       worker.postMessage({
@@ -69,25 +97,32 @@
       })
     } else {
       // use main thread
-      renderResult(window._util.processText(data.textArray))
+      generateResult(window._util.processText(data.textArray))
     }
   }
 
-  // Render result
-  function renderResult (result) {
+  // Generate result
+  function generateResult (result) {
     let resultHTML = ''
     let resultDownloadData = '\ufeff'
 
-    resultDownloadData += `词语,数量\r\n`
-    result.forEach(function (currentData) {
-      resultHTML += `<tr><td>${currentData.word}</td><td>${currentData.count}</td></tr>`
-      resultDownloadData += `${currentData.word},${currentData.count}\r\n`
+    resultDownloadData += `#,词语,数量\r\n`
+    result.forEach(function (currentData, index) {
+      resultHTML += `<tr><td class="table-value">${index + 1}</td><td class="table-key">${currentData.word}</td><td class="table-value">${currentData.count}</td></tr>`
+      resultDownloadData += `${index + 1},${currentData.word},${currentData.count}\r\n`
     })
 
     eleResultContainer.innerHTML = resultHTML
-    saveCsv(resultDownloadData)
+
+    eleDownloadButton.disabled = false
+    eleDownloadButton.onclick = function () {
+      saveCsv(resultDownloadData)
+    }
+
+    setFox(3)
   }
 
+  // Save files
   function saveCsv (data) {
     let a = document.createElement('a')
     let blob = new Blob([data], { type: 'text/csv;charset=utf-8;' })
@@ -100,5 +135,18 @@
     window.URL.revokeObjectURL(url)
     document.body.removeChild(a)
   }
+
+  // Set fox status
+  function setFox (status) {
+    let activeFox = eleFoxImgs[status]
+    Array.prototype.forEach.call(eleFoxImgs, function (ele) {
+      ele.style.display = 'none'
+    })
+
+    if (activeFox) {
+      activeFox.style.display = 'block'
+    }
+  }
+  setFox(0)
 
 })()
